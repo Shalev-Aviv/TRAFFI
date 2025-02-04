65f92578bc327bc3dc08d531b64468984ce191f8
Shalev Aviv <shalevaviv2005@gmail.com>
2025-02-05 01:04:42 +0200

    Add Backend files

---
+++ Backend/src/main/java/Shalev_Aviv/TRAFFI/TraffiApplication.java
@@ -0,0 +1,13 @@
+package Shalev_Aviv.TRAFFI;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TraffiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TraffiApplication.class, args);
+	}
+
+}
---
+++ Backend/src/test/java/Shalev_Aviv/TRAFFI/TraffiApplicationTests.java
@@ -0,0 +1,13 @@
+package Shalev_Aviv.TRAFFI;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TraffiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
