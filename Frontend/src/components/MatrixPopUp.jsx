18c5c4ac2456b6995940ee2447f40ac336a232e4
Shalev Aviv <shalevaviv2005@gmail.com>
2025-02-15 21:25:45 +0200

    Update MatrixPopUp.jsx

--- Frontend/src/components/MatrixPopUp.jsx
+++ Frontend/src/components/MatrixPopUp.jsx
@@ -1,9 +1,10 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect, useRef } from 'react';
+import ReactDOM from 'react-dom';
 import './MatrixPopUp.css';
 
 const MatrixPopUp = () => {
     const [showPopup, setShowPopup] = useState(false);
-    const [text, setText] = useState('');
+    const [text, setText] = useState("G = [\n  [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,],   # Node 1\n  [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,],   # Node 2\n  [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,],   # Node 3\n  [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,],   # Node 4\n  [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,],   # Node 5\n  [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,],   # Node 6\n  [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,],   # Node 7\n  [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,],   # Node 8\n  [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,],   # Node 9\n  [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,],   # Node 10\n  [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,],   # Node 11\n  [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,],   # Node 12\n  [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,],   # Node 13\n  [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,],   # Node 14\n  [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,],   # Node 15\n  [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,]    # Node 16\n]");
 
     const togglePopup = () => {
         setShowPopup(!showPopup);
@@ -14,7 +15,7 @@ const MatrixPopUp = () => {
     };
 
     const handleSend = () => {
-        const fileData = JSON.stringify({ message: text || "12345" });
+        const fileData = JSON.stringify({ message: text });
         const blob = new Blob([fileData], { type: 'application/json' });
         const url = URL.createObjectURL(blob);
         const a = document.createElement('a');
@@ -22,29 +23,28 @@ const MatrixPopUp = () => {
         a.download = 'message.json';
         a.click();
         URL.revokeObjectURL(url);
-        setShowPopup(false); // Close the popup after sending
-        // Note: Saving directly to a backend folder isn't possible from frontend code.
-        // This is a security restriction. You would need to send the data to a backend
-        // endpoint which then saves the file on the server.
-        // The current implementation allows the user to download the file.
-        };
-
-        return (
-        <div>
-            <button className="start-button" onClick={togglePopup}>Start</button>
-            {showPopup && (
-            <div className="popup">
-                <div className="popup-inner">
-                    <textarea type="text" value={text || "G\n[\n[ 1 , 1 , 1 , 0 , 0 ] # node 1\n[ 1 , 0 , 1 , 0 , 1 ] # node 2\n]"} onChange={handleTextChange} />
-                    <div className="popup-buttons">
-                        <button className='send' onClick={handleSend}>Send</button>
-                        <button className='close' onClick={togglePopup}>Close</button>
-                    </div>
-                </div>
-            </div>
-            )}
-        </div>
-        );
+        setShowPopup(false);
     };
 
-    export default MatrixPopUp;
+    return (
+        <div>
+            <button className="start-button" onClick={togglePopup}>Start</button>
+            {showPopup &&
+                ReactDOM.createPortal(
+                    <div className="popup">
+                        <div className="popup-inner">
+                            <textarea type="text" value={text} onChange={handleTextChange} />
+                            <div className="popup-buttons">
+                                <button className='send' onClick={handleSend}>Send</button>
+                                <button className='close' onClick={togglePopup}>Close</button>
+                            </div>
+                        </div>
+                    </div>,
+                    document.body
+                )
+            }
+        </div>
+    );
+};
+
+export default MatrixPopUp;
