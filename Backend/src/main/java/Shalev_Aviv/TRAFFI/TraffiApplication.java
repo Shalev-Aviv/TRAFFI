f05311eaef1f259111b2ea995c5cfe272a31222a
Shalev Aviv <shalevaviv2005@gmail.com>
2025-02-22 13:35:44 +0200

    Server runs

--- Backend/src/main/java/Shalev_Aviv/TRAFFI/TraffiApplication.java
+++ Backend/src/main/java/Shalev_Aviv/TRAFFI/TraffiApplication.java
@@ -1,19 +1,25 @@
 package Shalev_Aviv.TRAFFI;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
 
 @SpringBootApplication
+@RestController
 public class TraffiApplication {
-
+	
 	public static void main(String[] args) {
 		SpringApplication.run(TraffiApplication.class, args);
 	}
 
-	@PostMapping("/api/test")
-	public String test() {
-		return "Hello World";
+	@GetMapping("/")
+	public String index() {
+		return "Connected to the server!";
 	}
-
 }
